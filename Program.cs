d2d34206f05e614ad4487c20da917a60c7ae207b
Alex Yu <alexyu99126@gmail.com>
2016-08-22 16:10:37 -0700

    Updated core dependency to version 2.4.0.102

--- Program.cs
+++ Program.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Cantus.Core;
 using System.Threading;
 using System.IO;
-using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
